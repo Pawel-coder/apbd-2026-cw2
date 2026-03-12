2ec71a9446ff98f89bece25578dc91937356710a
s33980 <gm456HM32193!?@>
2026-03-12 09:54:28 +0100

    Added class StatisticsHelper with static method double CalculateMax(int[] values)

---
+++ Tutorial2/Tutorial2/StatisticsHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Tutorial2;
+
+public class StatisticsHelper
+{
+    public static double CalculateMax(int[] values)
+    {
+        int max = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] > max)
+                max = values[i];
+        }
+        return max;
+    }
+}
