7caf23ae37e323904870e096d2ca72b696139577
s33980 <gm456HM32193!?@>
2026-03-12 10:01:58 +0100

    In Program.cs added line on console showing Max value in {1,13,3}

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Tutorial2;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Max in [1,13,3] is:");
+Console.WriteLine(StatisticsHelper.CalculateMax(new int[3]{1,13,3}));
