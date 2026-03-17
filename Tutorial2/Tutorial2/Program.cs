e18e31184e7824ca3a44c14b7f9fb4637622d7be
Pawel-coder <s33980@pjwstk.edu.pl>
2026-03-17 18:32:11 +0100

    In Program.cs added line showing on console result of CalculateMin() for int[3]{2,3,4}.

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -6,4 +6,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(StatisticsHelper.CalculateAverage(new int[3]{1,13,3}));
 Console.WriteLine(StatisticsHelper.CalculateAverage(new int[3]{13,13,13}));
 Console.WriteLine(StatisticsHelper.CalculateAverage(new int[3]{2,3,4}));
+Console.WriteLine(StatisticsHelper.CalculateMin(new int[3]{4,5,6}));
 Console.WriteLine("Goodbye, World!");
