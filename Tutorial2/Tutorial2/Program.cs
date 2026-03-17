9103c013193d97e381da3106891ff7e0bb84578b
Pawel-coder <s33980@pjwstk.edu.pl>
2026-03-17 18:34:46 +0100

    In Program.cs added line showing on console result of CalculateMax() - CalculateMin() for int[3]{2,3,4}.

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -7,4 +7,5 @@ Console.WriteLine(StatisticsHelper.CalculateAverage(new int[3]{1,13,3}));
 Console.WriteLine(StatisticsHelper.CalculateAverage(new int[3]{13,13,13}));
 Console.WriteLine(StatisticsHelper.CalculateAverage(new int[3]{2,3,4}));
 Console.WriteLine(StatisticsHelper.CalculateMin(new int[3]{4,5,6}));
+Console.WriteLine(StatisticsHelper.CalculateMax(new int[3]{2,3,4})-StatisticsHelper.CalculateMin(new int[3]{2,3,4}));
 Console.WriteLine("Goodbye, World!");
